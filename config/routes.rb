66b244f255731d15584acfc23b9df1ccc981ba28
Leo Correa <lcorr005@gmail.com>
2013-06-30 22:55:52 -0400

    Authentication through devise...finally

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 AppDojoApi::Application.routes.draw do
 
-  post    'login'  => 'sessions#create', as: :login
-  delete  'logout' => 'sessions#destroy', as: :logout
-
-  resources :users, except: [:new, :edit]
+  root to: 'products#index'
 
 
+  scope :api, path: 'api' do
+    scope :v1, path: 'v1' do
+      devise_for :users, controllers: {:sessions => 'sessions'}, skip: [:sessions]
+      resources :users, except: [:new, :edit]
+    end
+  end
+
+  devise_scope :user do
+    post 'api/v1/users/sign_in' => 'sessions#create', as: :sign_in
+    delete 'api/v1/users/sign_out' => 'sessions#destroy', as: :sign_out
+  end
 end
