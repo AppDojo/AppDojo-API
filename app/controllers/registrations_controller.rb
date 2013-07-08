e122d3ef4166dae48d0bc6964d3b1f717ae8956a
Leo Correa <lcorr005@gmail.com>
2013-07-07 21:33:45 -0400

    New registration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,37 @@
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up resource_name, resource
+        return render json: {success: true, user: resource}
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        return render json: {success: true, user: resource}
+      end
+    else
+      clean_up_passwords resource
+      render json: {success: false, errors: resource.errors.full_messages}
+    end
+  end
+
+  def update
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    if resource.update_with_password(account_update_params)
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+            :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,24 @@ AppDojoApi::Application.routes.draw do
 
   scope :api, path: 'api' do
     scope :v1, path: 'v1' do
-      devise_for :users, controllers: {:sessions => 'sessions'}, skip: [:sessions]
+      devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}, skip: [:sessions, :registrations]
+
       resources :check_ins, except: [:new, :edit]
       resources :meetings, except: [:new, :edit]
-      resources :users, except: [:new, :edit]
+      resources :users, except: [:new, :edit, :create, :update, :destroy]
     end
   end
 
+  # POST   /api/v1/users(.:format)               registrations#create
+  # GET    /api/v1/users/sign_up(.:format)       registrations#new
+  # GET    /api/v1/users/edit(.:format)          registrations#edit
+  # PATCH  /api/v1/users(.:format)               registrations#update
+  # PUT    /api/v1/users(.:format)               registrations#update
+  # DELETE /api/v1/users(.:format)               registrations#destroy
   devise_scope :user do
-    post 'api/v1/users/sign_in' => 'sessions#create', as: :sign_in
-    delete 'api/v1/users/sign_out' => 'sessions#destroy', as: :sign_out
+    #put  'api/v1/users'             => 'registrations#update', as: :user_information_update
+    post 'api/v1/users'             => 'registrations#create', as: :user_registrations
+    post 'api/v1/users/sign_in'     => 'sessions#create', as: :sign_in
+    delete 'api/v1/users/sign_out'  => 'sessions#destroy', as: :sign_out
   end
 end
