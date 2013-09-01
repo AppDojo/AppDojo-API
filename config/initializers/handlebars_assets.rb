8dfcea6dfd68ba9d08940ff548cecb04611d8512
Leo Correa <lcorr005@gmail.com>
2013-09-01 19:56:01 -0400

    Fix for development

--- config/initializers/handlebars_assets.rb
+++ config/initializers/handlebars_assets.rb
@@ -1,3 +1,6 @@
-if "assets" == ENV["RAILS_GROUPS"] || ["development", "test"].include?(ENV["RAILS_ENV"])
+if defined?(HandlebarsAssets)
   HandlebarsAssets::Config.ember = true
 end
+#if "assets" == ENV["RAILS_GROUPS"] || ["development", "test"].include?(ENV["RAILS_ENV"])
+#  HandlebarsAssets::Config.ember = true
+#end
