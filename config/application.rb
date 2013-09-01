0ba8a11490e3d2b2ead1491249eb19786c0e0d73
Leo Correa <lcorr005@gmail.com>
2013-09-01 19:18:16 -0400

    We might have to uncomment this to work with heroku

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module AppDojoApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.secret_key_base = 'whoopsie'
+
+    #config.assets.initialize_on_precompile = true
   end
 end
